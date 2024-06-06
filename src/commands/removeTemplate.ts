18a2f2063882db55d15e3b30795ab58b4fa05928
zwkang <kang95630@gmail.com>
2024-06-06 17:19:26 +0800

    feat: support remove internal template

--- src/commands/removeTemplate.ts
+++ src/commands/removeTemplate.ts
@@ -3,15 +3,16 @@ import { removeTemplate } from '../removeTemplate'
 
 interface Args {
   name?: string
+  internal?: boolean
 }
 
 export const removeTemplateCommand: YargsCommandModule<{}, Args> = {
   command: 'remove-template [name]',
   describe: 'remove template',
-  builder: yargs => yargs.option('name', { type: 'string' }),
-  handler: ({ name }) => {
+  builder: yargs => yargs.option('name', { type: 'string' }).option('internal', { type: 'boolean', default: false }),
+  handler: ({ name, internal }) => {
     if (!name)
       return
-    removeTemplate(name)
+    removeTemplate(name, internal)
   },
 }
--- src/removeTemplate.ts
+++ src/removeTemplate.ts
@@ -1,12 +1,10 @@
-import path from 'node:path'
 import fsExtra from 'fs-extra'
 import { logger } from 'rslog'
 import colors from 'picocolors'
-import { getCacheFolder } from './share'
+import { resolveTemplate } from './share'
 
-export async function removeTemplate(templateName: string) {
-  const cacheFolder = getCacheFolder()
-  const templatePath = path.join(cacheFolder, templateName)
+export async function removeTemplate(templateName: string, internal: boolean = false) {
+  const templatePath = await resolveTemplate(templateName, { internal })
   const exist = await fsExtra.exists(templatePath)
   if (exist)
     await fsExtra.remove(templatePath)
